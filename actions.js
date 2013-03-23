57498db519df508051d28799fa33c760ac0682d5
netophp <netophp@gmail.com>
2013-03-23 12:29:24 -0600

    a

--- actions.js
+++ actions.js
@@ -2,10 +2,14 @@
 function inicio(){
 	
 	document.addEventListener("deviceready",function(){
-	var coords = coordenadas();
- 	var myLatlng = new google.maps.LatLng(coords['latitude'],coords['longitude']);
+		
+		navigator.geolocation.getCurrentPosition(onSuccess, onError);
+	
+	function onSuccess(position) {
+
+ 	var myLatlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
   	var mapOptions = {
-		zoom: 8,
+		zoom: 15,
 		center: myLatlng,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	  }
@@ -17,26 +21,22 @@ function inicio(){
       map: map,
       title:"Hello World!"
   });
+  
+  
+	}  function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+                'message: ' + error.message + '\n');
+    }
 	},false);
+	
+	
   
 }
 
-function coordenadas(){
-	var arr = [];
+/*function coordenadas(){
+	var arr = [];*/
 	
-	navigator.geolocation.getCurrentPosition(onSuccess, onError);
 	
-	function onSuccess(position) {
-    	arr['latitude']=position.coords.latitude;
-        arr['longitude']=position.coords.longitude;
-};
 
-// onError Callback receives a PositionError object
-//
-function onError(error) {
-    alert('code: '    + error.code    + '\n' +
-          'message: ' + error.message + '\n');
-}
-
-return arr;
-	}
+/*return arr;
+	}*/
